e1a5f066a3cf53e712d100d96382004f58fa88ae
Paul Cooke <paulgfcooke@gmail.com>
2019-10-10 11:12:28 +0100

    very basic app running

---
+++ project-2-folder/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+class App extends React.Component {
+  render() {
+    return <h1>The alternative times</h1>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
