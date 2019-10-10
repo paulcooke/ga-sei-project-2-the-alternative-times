0c6a0974437fd567dd05cdb7ba28369a2c1b50d7
Paul Cooke <paulgfcooke@gmail.com>
2019-10-10 11:22:36 +0100

    basic render of real news stories

--- project-2-folder/src/app.js
+++ project-2-folder/src/app.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 
 import Header from './common/Header'
+import DisplayRealNews from './components/DisplayRealNews'
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <DisplayRealNews />
+      </>
     )
   }
 }
--- project-2-folder/src/components/DisplayRealNews.js
+++ project-2-folder/src/components/DisplayRealNews.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+class DisplayRealNews extends React.Component {
+  render() {
+    return (
+      <h1>I am a real news article</h1>
+    )
+  }
+}
+
+export default DisplayRealNews
