68584d567f3f42cecb8852fac3cb6fc04ef3b61e
Paul Cooke <paulgfcooke@gmail.com>
2019-10-10 14:18:33 +0100

    storing unchanged fake headline from original get request

--- project-2-folder/src/components/DisplayNews.js
+++ project-2-folder/src/components/DisplayNews.js
@@ -8,7 +8,8 @@ class DisplayNews extends React.Component {
     this.state = {
       articles: null,
       shuffleIndex: 0,
-      error: ''
+      error: '',
+      fakeHeadline: ''
     }
     this.handleShuffle = this.handleShuffle.bind(this)
   }
@@ -20,32 +21,42 @@ class DisplayNews extends React.Component {
   getStory() {
     const newsKey = process.env.NEWSAPI_ACCESS_KEY
     axios.get(`https://newsapi.org//v2/top-headlines?country=gb&apiKey=${newsKey}`)
-      .then(res => this.setState({ articles: res.data.articles }))
+      .then(res => this.setState({ articles: res.data.articles, fakeHeadline: res.data.articles[0].title })) // has [0] to match the shuffle index start
       .catch(err => this.setState({ error: err.message }))
   }
 
   handleShuffle() {
-    this.setState({ shuffleIndex: Math.floor(Math.random() * this.state.articles.length) })
+    const shuffleValue = Math.floor(Math.random() * this.state.articles.length)
+    this.setState({ shuffleIndex: shuffleValue, fakeHeadline: this.state.articles[shuffleValue].title })
+  }
+
+  handleFake() {
+    // this.setState({ fakeHeadline: this.state. })
   }
 
   render() {
     const { articles, shuffleIndex } = this.state
     console.log(this.state)
     return (
-      <article>
-        <button onClick={this.handleShuffle}>Shuffle</button>
-        {!articles && !this.error && <h1>Loading...</h1>}
-        {articles &&
-          <>
-            <h2>{articles[shuffleIndex].title}</h2>
-            <div>
-              <div>{articles[shuffleIndex].description}</div>
-              <img src={articles[shuffleIndex].urlToImage} alt="Image not available"/>
-            </div>
-          </>
-        }
-
-      </article>
+      <>
+        <article>
+          <button onClick={this.handleShuffle}>Shuffle</button>
+          {!articles && !this.error && <h1>Loading...</h1>}
+          {articles &&
+            <>
+              <h2>{articles[shuffleIndex].title}</h2>
+              <div>
+                <div>{articles[shuffleIndex].description}</div>
+                <img src={articles[shuffleIndex].urlToImage} alt="Image not available"/>
+              </div>
+            </>
+          }
+        </article>
+        <article>
+          <button onClick={this.handleFake}>change news</button>
+          <h2>placehoilder</h2>
+        </article>
+      </>
     )
   }
 }
