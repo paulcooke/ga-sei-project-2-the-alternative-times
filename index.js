aedcb04c3fb24eafd4f02f66ac296a56a5d70415
Paul Cooke <paulgfcooke@gmail.com>
2019-10-23 10:43:15 +0100

    deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.POR || 4000
+const port = process.env.PORT || 4000
 
 app.use(express.static(`${__dirname}/dist`))
 
