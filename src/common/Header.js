10954448d5be792d629f7caa0be4b390a8859792
Paul Cooke <paulgfcooke@gmail.com>
2019-11-30 13:07:45 +0000

    added moment for the date

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import moment from 'moment'
+
+const today = moment().format('dddd, MMMM Do')
 
 const Header = () => (
   <>
     <h1>The Alternative Times</h1>
     <div className="dateBar">
       <p>Volume: MMXIX</p>
-      <p>Friday 11 October 2019</p>
+      <p>{today}</p>
       <p>£2.50</p>
     </div>
   </>
